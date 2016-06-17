2e6c5919a7738b664a61c48e2a267fa1244b985c
codewizard <mikemcguire.dev@gmail.com>
2016-06-16 19:34:52 -0600

    Initializing repository

---
+++ index.android.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  TextInput,
+  ListView,
+  Text,
+  View
+} from 'react-native';
+
+class Githubr extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput placeholder="Type here to search for repositories" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Githubr', () => Githubr);
